f17980b84962fffbc4e5905bbe9ba42c22514ff4
Jan@Digi <jvbiervliet@gmail.com>
2016-01-25 16:00:22 +0100

    Add Repository class

---
+++ src/TodoBundle/Entity/Repo/TodoRepository.php
@@ -0,0 +1,13 @@
+<? php
+
+namespace TodoBundle\Entity\Repo;
+
+use Doctrine\ORM\EntityRepository;
+
+class TodoRepository extends EntityRepository {
+  public function updateFinishedStateOfId($id, $state) {
+    return $this->getEntityManager()
+      ->createQuery("UPDATE todo_item SET finihed = $state WHERE id = $id")
+      ->getResult();
+  }
+}
